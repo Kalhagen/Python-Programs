954dd56f3996b417ced08f2e5416685b5e8c63d8
SoftwareLiteracy <JosephPark@SoftwareLiteracyFoundation.org>
2014-01-04 09:10:21 -0500

    Create program_1.py

---
+++ program_1.py
@@ -0,0 +1,8 @@
+def main():
+    myName = input('Type in your name: ')
+
+    print( 'Your name is', myName )
+    print( myName, 'is', len(myName), 'letters long.')
+
+if __name__ == "__main__":
+    main()
